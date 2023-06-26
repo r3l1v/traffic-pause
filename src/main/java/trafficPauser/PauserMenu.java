6b669cb2043d4e75a2987837f80a4e83175a20af
r3l1v <czsindragcz@gmail.com>
2023-06-26 15:28:38 +0200

    New manu with references to objects in map

--- src/main/java/trafficPauser/PauserMenu.java
+++ src/main/java/trafficPauser/PauserMenu.java
@@ -18,43 +18,44 @@ import java.text.NumberFormat;
 import javax.swing.text.NumberFormatter;
 import java.awt.event.ActionEvent;
 
-
-class PauserMenu implements MenuListener{
+class PauserMenu implements MenuListener {
 
     private JMenu menuButton;
     public static Logging logging;
     public static MontoyaApi api;
     private Registration menu;
     public static ConfigurableSettings globalSettings;
-    public static Set<ToolType> stopped_tools = new HashSet<>(); 
+    public static Set<ToolType> stopped_tools = new HashSet<>();
 
     public static boolean traffic_switch_string = false;
     public static boolean traffic_switch_regex = false;
 
     public static LinkedHashMap<String, Boolean> regex_match_tools = new LinkedHashMap<String, Boolean>();
-    public static LinkedHashMap<String, Boolean> string_match_tools = new LinkedHashMap<String, Boolean>();;
+    public static LinkedHashMap<String, Boolean> string_match_tools = new LinkedHashMap<String, Boolean>();
 
-    public PauserMenu(MontoyaApi api){
+    public PauserMenu(MontoyaApi api) {
         this.logging = api.logging();
         this.api = api;
+        populateTools();
         this.globalSettings = new ConfigurableSettings();
         this.menuButton = new JMenu("Traffic Pauser");
         this.menuButton.addMenuListener(this);
         this.menu = api.userInterface().menuBar().registerMenu(this.menuButton);
-        populateTools();
-    } 
+    }
 
-    public void populateTools(){
-        String[] tools = {"REPEATER","INTRUDER","SCANNER","PROXY", "EXTENSIONS"};
-        for(String s:tools){
+    public void populateTools() {
+        String[] tools = { "REPEATER", "INTRUDER", "SCANNER", "PROXY", "EXTENSIONS" };
+        for (String s : tools) {
             regex_match_tools.put(s, false);
             string_match_tools.put(s, false);
         }
     }
 
-    public void menuDeselected(MenuEvent e) { }
+    public void menuDeselected(MenuEvent e) {
+    }
 
-    public void menuCanceled(MenuEvent e) { }
+    public void menuCanceled(MenuEvent e) {
+    }
 
     public void menuSelected(MenuEvent e) {
         globalSettings.showSettings();
@@ -67,6 +68,8 @@ class PauserMenu implements MenuListener{
 
 class ConfigurableSettings {
     private LinkedHashMap<String, String> settings;
+    public LinkedHashMap<JCheckBox, String> tools_regex = new LinkedHashMap<JCheckBox, String>();
+    public LinkedHashMap<JCheckBox, String> tools_string = new LinkedHashMap<JCheckBox, String>();
     private NumberFormatter onlyInt;
 
     ConfigurableSettings() {
@@ -74,14 +77,13 @@ class ConfigurableSettings {
         put("Pause all traffic", false);
         put("Pause all traffic on string match", false);
         put("String to match", "string");
-        put("regex_match_tools", false);
+        put("string_match_tools", false);
         put("Pause all traffic on Regex match", false);
         put("Regex to match", "regex");
-        put("string_match_tools", false);
-        
+        put("regex_match_tools", false);
 
-        for(String key: settings.keySet()) {
-            //load extension settings if set
+        for (String key : settings.keySet()) {
+            // load extension settings if set
             PersistedObject myExtensionData = PauserMenu.api.persistence().extensionData();
             String value = myExtensionData.getString(key);
             if (value != null) {
@@ -95,6 +97,8 @@ class ConfigurableSettings {
         onlyInt.setMinimum(-1);
         onlyInt.setMaximum(Integer.MAX_VALUE);
         onlyInt.setAllowsInvalid(false);
+
+        init_tools_checkboxes();
     }
 
     private ConfigurableSettings(ConfigurableSettings base) {
@@ -103,8 +107,8 @@ class ConfigurableSettings {
     }
 
     private void printSettings() {
-        for(String key: settings.keySet()) {
-            PauserMenu.out(key + ": "+settings.get(key));
+        for (String key : settings.keySet()) {
+            PauserMenu.out(key + ": " + settings.get(key));
         }
     }
 
@@ -112,11 +116,9 @@ class ConfigurableSettings {
         String encoded;
         if (value instanceof Boolean) {
             encoded = String.valueOf(value);
-        }
-        else if (value instanceof Integer) {
+        } else if (value instanceof Integer) {
             encoded = String.valueOf(value);
-        }
-        else {
+        } else {
             encoded = "\"" + ((String) value).replace("\\", "\\\\").replace("\"", "\\\"") + "\"";
         }
         return encoded;
@@ -128,13 +130,13 @@ class ConfigurableSettings {
 
     private void put(String key, Object value) {
         settings.put(key, encode(value));
-        switch(key){
+        switch (key) {
             case "Pause all traffic on string match":
-                if(!(Boolean)value){
+                if (!(Boolean) value) {
                     PauserMenu.traffic_switch_string = false;
                 }
             case "Pause all traffic on Regex match":
-                if(!(Boolean)value){
+                if (!(Boolean) value) {
                     PauserMenu.traffic_switch_regex = false;
                 }
         }
@@ -142,7 +144,7 @@ class ConfigurableSettings {
 
     String getString(String key) {
         String decoded = settings.get(key);
-        decoded = decoded.substring(1, decoded.length()-1).replace("\\\"", "\"").replace("\\\\", "\\");
+        decoded = decoded.substring(1, decoded.length() - 1).replace("\\\"", "\"").replace("\\\\", "\\");
         return decoded;
     }
 
@@ -152,10 +154,9 @@ class ConfigurableSettings {
 
     boolean getBoolean(String key) {
         String val = settings.get(key);
-        if (val.equals("true") ) {
+        if (val.equals("true")) {
             return true;
-        }
-        else if (val.equals("false")){
+        } else if (val.equals("false")) {
             return false;
         }
         throw new RuntimeException();
@@ -165,44 +166,83 @@ class ConfigurableSettings {
         String val = settings.get(key);
         if (val.equals("true") || val.equals("false")) {
             return "boolean";
-        }
-        else if (val.startsWith("\"")) {
+        } else if (val.startsWith("\"")) {
             return "string";
-        }
-        else {
+        } else {
             return "number";
         }
     }
 
-    void setupTools(JPanel panel, GridBagConstraints c, Integer row,HashMap<String, Object> configured){
-        
+    public void init_tools_checkboxes() {
+        JCheckBox box;
+        for (String key : PauserMenu.regex_match_tools.keySet()) {
+            box = new JCheckBox();
+            box.setSelected(PauserMenu.regex_match_tools.get(key));
+            tools_regex.put(box, key);
+        }
 
-        //adding left most label to the grid
+        for (String key : PauserMenu.string_match_tools.keySet()) {
+            box = new JCheckBox();
+            box.setSelected(PauserMenu.regex_match_tools.get(key));
+            tools_string.put(box, key);
+        }
+    }
+
+    void setupTools(JPanel panel, GridBagConstraints c, Integer row, Boolean map_switch) {
+
+        //map_switch 0 - regex hashmap
+        //           1 - string hashmap
+
+        // adding left most label to the grid
         c.gridx = 0;
         c.gridy = row;
         c.gridwidth = 1;
         JLabel label = new JLabel("Pause for: ");
         panel.add(label, c);
 
-        //adding labels of tools to the grid
+        // adding labels of tools to the grid
         c.gridx = 1;
-        for(String key: PauserMenu.regex_match_tools.keySet()){
-            label = new JLabel( key.substring(0, 1) + key.substring(1).toLowerCase());
-            c.insets = new Insets(2,0,5,0);
-            panel.add(label,c);
-            c.gridx += 2;
 
+        LinkedHashMap<JCheckBox, String> map = tools_regex;
+        if(map_switch){
+            map = tools_string;
         }
-        c.gridx = 2;
-        //adding checkboxes to the grid
-        JCheckBox box;
-        for(Boolean value:PauserMenu.regex_match_tools.values()){
-            box = new JCheckBox();
-            box.setSelected(value);
-            c.insets = new Insets(2,0,5,20);
-            panel.add(box, c);
+
+        for (JCheckBox key : map.keySet()) {
+            label = new JLabel(map.get(key).substring(0, 1) + map.get(key).substring(1).toLowerCase());
+            c.insets = new Insets(2, 0, 5, 0);
+            panel.add(label, c);
             c.gridx += 2;
         }
+
+        c.gridx = 2;
+
+        // adding checkboxes to the grid
+
+        for (JCheckBox key : map.keySet()) {
+            c.insets = new Insets(2, 0, 5, 20);
+            panel.add(key, c);
+            c.gridx += 2;
+        }
+
+        // for (String key : PauserMenu.regex_match_tools.keySet()) {
+        //     label = new JLabel(key.substring(0, 1) + key.substring(1).toLowerCase());
+        //     c.insets = new Insets(2, 0, 5, 0);
+        //     panel.add(label, c);
+        //     c.gridx += 2;
+
+        // }
+        // c.gridx = 2;
+        // // adding checkboxes to the grid
+        // JCheckBox box;
+        // for (Boolean value : PauserMenu.regex_match_tools.values()) {
+        //     box = new JCheckBox();
+        //     box.setSelected(value);
+        //     c.insets = new Insets(2, 0, 5, 20);
+        //     panel.add(box, c);
+
+        //     c.gridx += 2;
+        // }
     }
 
     ConfigurableSettings showSettings() {
@@ -215,54 +255,58 @@ class ConfigurableSettings {
         JLabel label;
 
         HashMap<String, Object> configured = new HashMap<>();
-        for(String key: settings.keySet()) {
+        for (String key : settings.keySet()) {
             String type = getType(key);
 
-            if(key.endsWith("tools")){
-                
-                setupTools(panel, c, row, configured);
+            if(key.equals("regex_match_tools")){
+                setupTools(panel, c, row, false);
+                row++;
+                continue;
+            }
+            else if(key.equals("string_match_tools")){
+                setupTools(panel, c, row, true);
                 row++;
                 continue;
             }
 
-            label = new JLabel("\n"+key+": ");
+            label = new JLabel("\n" + key + ": ");
 
             c.fill = GridBagConstraints.HORIZONTAL;
             c.gridx = column;
             c.gridy = row;
-            c.insets = new Insets(0,0,0,100);
-            panel.add(label,c);
+            c.insets = new Insets(0, 0, 0, 100);
+            panel.add(label, c);
 
             column++;
-            
-            //adding checkbox to the grid
+
+            // adding checkbox to the grid
             if (type.equals("boolean")) {
                 JCheckBox box = new JCheckBox();
                 box.setSelected(getBoolean(key));
                 c.gridwidth = 10;
-                //c.insets = new Insets(0,0,0,300);
+                // c.insets = new Insets(0,0,0,300);
                 c.gridx = column;
-                panel.add(box,c);
+                panel.add(box, c);
 
                 configured.put(key, box);
             }
-            //adding number to the grid
-            else if (type.equals("number")){
+            // adding number to the grid
+            else if (type.equals("number")) {
                 JTextField box = new JFormattedTextField(onlyInt);
                 box.setText(String.valueOf(getInt(key)));
                 c.gridwidth = 10;
-                //c.insets = new Insets(0,0,0,300);
+                // c.insets = new Insets(0,0,0,300);
                 c.gridx = column;
-                panel.add(box,c);
+                panel.add(box, c);
 
                 configured.put(key, box);
             }
             // adding Text field to the grid
             else {
                 JTextField box = new JTextField(getString(key));
-                //c.insets = new Insets(0,0,0,0);
+                // c.insets = new Insets(0,0,0,0);
                 c.gridx = column;
-                panel.add(box,c);
+                panel.add(box, c);
                 configured.put(key, box);
             }
 
@@ -270,21 +314,20 @@ class ConfigurableSettings {
             row++;
         }
 
-        int result = JOptionPane.showConfirmDialog(null, panel, "Traffic Pauser", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        int result = JOptionPane.showConfirmDialog(null, panel, "Traffic Pauser", JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE);
         if (result == JOptionPane.OK_OPTION) {
-            for(String key: configured.keySet()) {
+            for (String key : configured.keySet()) {
                 Object val = configured.get(key);
                 if (val instanceof JCheckBox) {
                     val = ((JCheckBox) val).isSelected();
-                }
-                else if (val instanceof JFormattedTextField) {
+                } else if (val instanceof JFormattedTextField) {
                     val = Integer.parseInt(((JFormattedTextField) val).getText().replace(",", ""));
-                }
-                else {
+                } else {
                     val = ((JTextField) val).getText();
                 }
                 put(key, val);
-                //save extension settings
+                // save extension settings
                 PersistedObject myExtensionData = PauserMenu.api.persistence().extensionData();
                 myExtensionData.setString(key, encode(val));
             }
@@ -294,7 +337,5 @@ class ConfigurableSettings {
 
         return null;
     }
-
-
 
 }
